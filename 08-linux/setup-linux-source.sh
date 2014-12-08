f54efd9c964994861fbc7967f4eae25247886ed6
Kevin Leake <kevinleake01@gmail.com>
2014-12-08 23:07:39 +0000

    Update setup-linux-source.sh

--- 08-linux/setup-linux-source.sh
+++ 08-linux/setup-linux-source.sh
@@ -11,29 +11,25 @@
 # --- LINUX FROM SCRATCH ---
 mkdir 01-lfs
 cd 01-lfs
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6/LFS-BOOK-7.6-NOCHUNKS.html
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6/LFS-BOOK.pdf
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6/LFS-BOOK-7.6.tar.bz2
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6/wget-list -O LFS-7.6-wget-list.txt
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd-NOCHUNKS.html
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd.pdf
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd.tar.bz2
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/wget-list -O LFS-7.6-systemd-wget-list.txt
-wget http://www.linuxfromscratch.org/lfs/downloads/7.6/lfs-bootscripts-20140815.tar.bz2
-wget http://www.linuxfromscratch.org/blfs/downloads/7.6/blfs-book-7.6-html.tar.bz2
-wget http://www.linuxfromscratch.org/blfs/downloads/7.6/BLFS-BOOK-7.6-nochunks.html
-wget http://www.linuxfromscratch.org/blfs/downloads/7.6/blfs-bootscripts-20140919.tar.bz2
-git clone git://git.clfs.org/cross-lfs.git
+svn co svn://wiki.linuxfromscratch.org/lfs/tags/7.6 lfs-7.6
+svn co svn://wiki.linuxfromscratch.org/lfs/tags/7.6-systemd lfs-systemd-7.6
+svn co svn://wiki.linuxfromscratch.org/lfs/trunk lfs-latest
+svn co svn://wiki.linuxfromscratch.org/blfs/tags/7.6 blfs-7.6
+svn co svn://wiki.linuxfromscratch.org/blfs/tags/7.6-systemd blfs-systemd-7.6
+svn co svn://wiki.linuxfromscratch.org/blfs/trunk blfs-latest
+git clone git://git.clfs.org/cross-lfs.git clfs
 git clone git://git.clfs.org/clfs-sysroot.git
 git clone git://git.clfs.org/clfs-embedded.git
 git clone git://git.clfs.org/bootscripts-standard.git clfs-bootscripts-standard
 git clone git://git.clfs.org/bootscripts-embedded.git clfs-bootscripts-embedded
 git clone git://git.clfs.org/units.git clfs-systemd-units
-wget http://cblfs.cross-lfs.org/index.php/Main_Page -O CBLFS-index.html
-wget http://www.linuxfromscratch.org/alfs/downloads/jhalfs/stable/jhalfs-2.3.2.tar.bz2
-svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/tags/2.3.2 SVN-jhalfs-2.3.2
-svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/trunk SVN-jhalfs-latest
-wget http://linuxfromscratch.org/hints/downloads/hints.tar.bz2 -O LFS-Hints.tar.bz2
+wget http://cblfs.clfs.org/index.php/Main_Page -O CBLFS-index.html
+svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/tags/2.3.2 jhalfs-2.3.2
+svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/trunk jhalfs-latest
+svn co svn://wiki.linuxfromscratch.org/hlfs/tags/release-0.6 hlfs-0.6
+svn co svn://wiki.linuxfromscratch.org/hlfs/trunk hlfs-latest
+svn co svn://wiki.linuxfromscratch.org/hints/trunk lfs-hints
+svn co svn://wiki.linuxfromscratch.org/livecd/trunk live-cd
 cd ../
 
 # --- T2-SDE ---
@@ -43,19 +39,9 @@ wget http://www.t2-project.org/handbook/html/t2-book.html
 wget http://www.t2-project.org/handbook/ps+pdf/t2-handbook.pdf
 wget http://www.t2-project.org/handbook/ps+pdf/t2-handbook.ps
 wget http://www.t2-project.org/pr/ProjectFlyer2006en.pdf
-wget http://dl.t2-project.org/source/t2-2.1.0.tar.bz2
-wget http://dl.t2-project.org/source/t2-2.1.1.1.tar.bz2
-wget http://dl.t2-project.org/source/t2-2.1.1.tar.bz2
-wget http://dl.t2-project.org/source/t2-2.2.0-rc.tar.bz2
-wget http://dl.t2-project.org/source/t2-6.0.0.tar.bz2
-wget http://dl.t2-project.org/source/t2-6.0.1.tar.bz2
-wget http://dl.t2-project.org/source/t2-6.0.2.tar.bz2
-wget http://dl.t2-project.org/source/t2-6.0.3.tar.bz2
-wget http://dl.t2-project.org/source/t2-7.0-rc2.tar.bz2
-wget http://dl.t2-project.org/source/t2-8.0.tar.bz2
-svn co http://svn.exactcode.de/t2/trunk/ SVN-T2-Current
-svn co http://svn.exactcode.de/t2/tags/8.0/ SVN-T2-8.0
-svn co http://svn.exactcode.de/t2-handbook/ SVN-T2-Handbook
+svn co http://svn.exactcode.de/t2/tags/8.0/ T2-8.0
+svn co http://svn.exactcode.de/t2/trunk/ T2-latest
+svn co http://svn.exactcode.de/t2-handbook/ T2-Handbook
 git clone git://github.com/arete/t2.git ARETE-T2
 git clone git://github.com/arete/t2-hostkvm-config.git ARETE-T2-Hostkvm-Config
 git clone git://github.com/arete/t2-xfce-config.git ARETE-T2-Xfce-Config
