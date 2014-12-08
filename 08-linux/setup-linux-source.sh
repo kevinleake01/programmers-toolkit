faae4b2eb499aa74d4403944ba7748cd4f0cf0bd
Kevin Leake <kevinleake01@gmail.com>
2014-12-08 21:19:46 +0000

    Update setup-linux-source.sh

--- 08-linux/setup-linux-source.sh
+++ 08-linux/setup-linux-source.sh
@@ -17,7 +17,7 @@ wget http://www.linuxfromscratch.org/lfs/downloads/7.6/LFS-BOOK-7.6.tar.bz2
 wget http://www.linuxfromscratch.org/lfs/downloads/7.6/wget-list -O LFS-7.6-wget-list.txt
 wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd-NOCHUNKS.html
 wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd.pdf
-wget http://www.linuxfromscratch.org/lfs/downloads/7.5-systemd/LFS-BOOK-7.6-systemd.tar.bz2
+wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/LFS-BOOK-7.6-systemd.tar.bz2
 wget http://www.linuxfromscratch.org/lfs/downloads/7.6-systemd/wget-list -O LFS-7.6-systemd-wget-list.txt
 wget http://www.linuxfromscratch.org/lfs/downloads/7.6/lfs-bootscripts-20140815.tar.bz2
 wget http://www.linuxfromscratch.org/blfs/downloads/7.6/blfs-book-7.6-html.tar.bz2
@@ -26,11 +26,14 @@ wget http://www.linuxfromscratch.org/blfs/downloads/7.6/blfs-bootscripts-2014091
 git clone git://git.clfs.org/cross-lfs.git
 git clone git://git.clfs.org/clfs-sysroot.git
 git clone git://git.clfs.org/clfs-embedded.git
-git clone git://git.clfs.org/bootscripts-standard.git clfs-bootscripts-standard.git
-git clone git://git.clfs.org/bootscripts-embedded.git clfs-bootscripts-embedded.git
-git clone git://git.clfs.org/units.git clfs-systemd-units.git
+git clone git://git.clfs.org/bootscripts-standard.git clfs-bootscripts-standard
+git clone git://git.clfs.org/bootscripts-embedded.git clfs-bootscripts-embedded
+git clone git://git.clfs.org/units.git clfs-systemd-units
 wget http://cblfs.cross-lfs.org/index.php/Main_Page -O CBLFS-index.html
 wget http://www.linuxfromscratch.org/alfs/downloads/jhalfs/stable/jhalfs-2.3.2.tar.bz2
+svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/tags/2.3.2 SVN-jhalfs-2.3.2
+svn co svn://wiki.linuxfromscratch.org/alfs/jhalfs/trunk SVN-jhalfs-latest
+wget http://linuxfromscratch.org/hints/downloads/hints.tar.bz2 -O LFS-Hints.tar.bz2
 cd ../
 
 # --- T2-SDE ---
@@ -51,8 +54,7 @@ wget http://dl.t2-project.org/source/t2-6.0.3.tar.bz2
 wget http://dl.t2-project.org/source/t2-7.0-rc2.tar.bz2
 wget http://dl.t2-project.org/source/t2-8.0.tar.bz2
 svn co http://svn.exactcode.de/t2/trunk/ SVN-T2-Current
-svn co http://svn.exactcode.de/t2/branches/8.0/ SVN-T2-Branch-8.0
-svn co http://svn.exactcode.de/t2/tags/8.0/ SVN-T2-Tags-8.0
+svn co http://svn.exactcode.de/t2/tags/8.0/ SVN-T2-8.0
 svn co http://svn.exactcode.de/t2-handbook/ SVN-T2-Handbook
 git clone git://github.com/arete/t2.git ARETE-T2
 git clone git://github.com/arete/t2-hostkvm-config.git ARETE-T2-Hostkvm-Config
@@ -61,8 +63,6 @@ git clone git://github.com/arete/t2-lxmail-config.git ARETE-T2-Lxmail-Config
 git clone git://github.com/arete/build.git ARETE-T2-Build
 git clone git://github.com/arete/utility.git ARETE-T2-Utility
 git clone git://github.com/masterkorp/LuaBash.git
-wget http://distro.ibiblio.org/quirky/t2/t2-8.0rc-tpup-readme.txt
-wget http://distro.ibiblio.org/quirky/t2/t2-8.0rc-tpup.tar.bz2
 cd ../
 
 # --- ABORIGINAL LINUX ---
