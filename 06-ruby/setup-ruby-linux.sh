7bfbc19a00cef51fe818308a78c8a9c2c1d16747
Kevin Leake <kevinleake01@gmail.com>
2014-12-09 22:58:45 +0000

    Update setup-ruby-linux.sh

--- 06-ruby/setup-ruby-linux.sh
+++ 06-ruby/setup-ruby-linux.sh
@@ -3,23 +3,23 @@
 # This script will download various Ruby programming language
 # documentation and goodies to your computer.
 #
-# This script requires Subversion (SVN), Git and Hg (Mercurial)
+# This script requires Subversion (SVN), Git, and Hg (Mercurial)
 # installed on your computer.
 #
 #
 # --- RUBY-LANG AND RUBYGEMS ---
 mkdir 01-ruby
 cd 01-ruby
-wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.1.tar.gz
+wget http://ftp.ruby-lang.org/pub/ruby/stable-snapshot.tar.xz
 git clone http://github.com/rubygems/rubygems.git
-wget http://production.cf.rubygems.org/rubygems/rubygems-update-2.2.2.gem
 cd ../
 
 # --- RUBY-DOCS ---
 mkdir 02-rubydoc
 cd 02-rubydoc
-wget http://ruby-doc.org/downloads/ruby_2_1_1_core_rdocs.tgz
-wget http://ruby-doc.org/downloads/ruby_2_1_1_stdlib_rdocs.tgz
+wget http://ruby-doc.org/downloads/ruby_2_1_5_core_rdocs.tgz
+wget http://ruby-doc.org/downloads/ruby_2_1_5_stdlib_rdocs.tgz
+git clone http://github.com/rdoc/rdoc.git
 cd ../
 
 # --- OTHER ---
