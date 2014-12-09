8435415fb902208e36f22b5bcdf6f40dd028502f
Kevin Leake <kevinleake01@gmail.com>
2014-12-09 20:18:40 +0000

    Update setup-d-linux.sh

--- 02-d/setup-d-linux.sh
+++ 02-d/setup-d-linux.sh
@@ -3,7 +3,7 @@
 # This script will download various D programming language
 # documentation and goodies to your computer.
 #
-# This script requires Subversion (SVN), Git and Hg (Mercurial)
+# This script requires Subversion (SVN), Git, and Hg (Mercurial)
 # installed on your computer.
 #
 # --- DMD ---
@@ -16,16 +16,15 @@ git clone git://github.com/D-Programming-Language/dlang.org.git
 git clone git://github.com/D-Programming-Language/installer.git
 git clone git://github.com/D-Programming-Language/tools.git
 wget http://ftp.digitalmars.com/dmd.1.076.zip
-wget http://ftp.digitalmars.com/dmd.2.065.0.zip
-wget http://ftp.digitalmars.com/dmdscript.zip
-wget http://www.prowiki.org/upload/duser/spec_DMD_1.00.pdf
-wget http://digitalmars.com/d/2.0/dlangspec.mobi
+wget http://ftp.digitalmars.com/dmd.2.066.1.zip
 cd ../
 
 # --- GCC (GDC) ---
 mkdir 02-gdc
 cd 02-gdc
-git clone git://github.com/D-Programming-GDC/GDC.git
+git clone git://github.com/D-Programming-GDC/GDC.git gdc
+git clone git://github.com/D-Programming-GDC/gdcproject.git
+git clone git://github.com/D-Programming-GDC/GDMD.git gdmd
 cd ../
 
 # --- LLVM/LDC ---
@@ -41,7 +40,7 @@ cd ../
 # --- TANGO-D ---
 mkdir 04-tango
 cd 04-tango
-svn checkout http://svn.dsource.org/projects/tango/trunk tango
+svn co http://svn.dsource.org/projects/tango/trunk tango
 cd ../
 
 # --- OTHER ---
