db634ace723f0f5808beacf146a78b3c6ed1c5b3
Kevin Leake <kevinleake01@gmail.com>
2014-12-09 21:28:12 +0000

    Update setup-python-linux.sh

--- 05-python/setup-python-linux.sh
+++ 05-python/setup-python-linux.sh
@@ -10,10 +10,10 @@
 # --- PYTHON.ORG ---
 mkdir 01-python
 cd 01-python
-wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
-wget https://www.python.org/ftp/python/3.4.0/Python-3.4.0.tar.xz
-wget https://docs.python.org/2/archives/python-2.7.6-docs-pdf-a4.tar.bz2
-wget https://docs.python.org/3/archives/python-3.4.0-docs-pdf-a4.tar.bz2
+wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tar.xz
+wget https://www.python.org/ftp/python/3.4.2/Python-3.4.2.tar.xz
+wget https://docs.python.org/2/archives/python-2.7.8-docs-pdf-a4.tar.bz2
+wget https://docs.python.org/3/archives/python-3.4.2-docs-pdf-a4.tar.bz2
 cd ../
 
 # --- OTHER ---
