22d783685d945cba52379442f0e7e2e61fca8170
Kevin Leake <kevinleake01@gmail.com>
2015-01-04 11:15:56 +0000

    Update setup-nim-linux.sh

--- 10-nim/setup-nim-linux.sh
+++ 10-nim/setup-nim-linux.sh
@@ -9,19 +9,27 @@
 # --- NIM-LANG ---
 mkdir 01-nim
 cd 01-nim
-git clone git://github.com/Araq/Nimrod.git
-git clone https://github.com/nim-lang/nim-buildbot.git
-git clone https://github.com/nim-lang/nimble.git
-git clone https://github.com/nim-lang/nimforum.git
-git clone https://github.com/nim-lang/packages.git
-git clone https://github.com/nim-lang/csources.git
-git clone https://github.com/nim-lang/c2nim.git
-git clone https://github.com/nim-lang/nimbuild.git
-git clone https://github.com/nim-lang/kickstart.git
-git clone https://github.com/nim-lang/Aporia.git
-git clone https://github.com/nim-lang/python.git
-git clone https://github.com/nim-lang/tcl.git
-git clone https://github.com/nim-lang/nits.git
+git clone http://github.com/Araq/Nim.git
+git clone http://github.com/nim-lang/nim-buildbot.git
+git clone http://github.com/nim-lang/nimble.git
+git clone http://github.com/nim-lang/nimforum.git
+git clone http://github.com/nim-lang/packages.git
+git clone http://github.com/nim-lang/csources.git
+git clone http://github.com/nim-lang/c2nim.git
+git clone http://github.com/nim-lang/nimbuild.git
+git clone http://github.com/nim-lang/kickstart.git
+git clone http://github.com/nim-lang/Aporia.git
+git clone http://github.com/nim-lang/python.git
+git clone http://github.com/nim-lang/tcl.git
+git clone http://github.com/nim-lang/nits.git
+git clone http://github.com/nim-lang/gtk2.git
+git clone http://github.com/nim-lang/opengl.git
+git clone http://github.com/nim-lang/nim-zmq.git
+git clone http://github.com/nim-lang/cairo.git
+git clone http://github.com/nim-lang/x11.git
+git clone http://github.com/nim-lang/lua.git
+git clone http://github.com/nim-lang/mongo.git
+git clone http://github.com/nim-lang/opencl.git
 cd ../
 
 
