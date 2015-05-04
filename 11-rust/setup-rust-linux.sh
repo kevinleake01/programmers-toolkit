3a7e81e0ba217d68cb9441a81360fb44a27572a6
kevinleake01 <kevinleake01@gmail.com>
2015-05-04 12:07:15 +0000

    Rust Programming

---
+++ 11-rust/setup-rust-linux.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+mkdir 01-rust
+cd 01-rust
+git clone https://github.com/rust-lang/rust.git
+git clone https://github.com/rust-lang/cargo.git
+git clone https://github.com/rust-lang/crates.io.git
+git clone https://github.com/rust-lang/crates.io-index.git
+git clone https://github.com/rust-lang/uuid.git
+git clone https://github.com/rust-lang/rust.vim.git
+git clone https://github.com/rust-lang/rustc-serialize.git
+git clone https://github.com/rust-lang/threadpool.git
+git clone https://github.com/rust-lang/rust-by-example.git
+git clone https://github.com/rust-lang/term.git
+git clone https://github.com/rust-lang/regex.git
+git clone https://github.com/rust-lang/getopts.git
+git clone https://github.com/rust-lang/time.git
+git clone https://github.com/rust-lang/rand.git
+git clone https://github.com/rust-lang/libc.git
+git clone https://github.com/rust-lang/num.git
+git clone https://github.com/rust-lang/tempdir.git
+git clone https://github.com/rust-lang/rlibc.git
+git clone https://github.com/rust-lang/bitflags.git
+git clone https://github.com/rust-lang/glob.git
+git clone https://github.com/rust-lang/semver.git
+git clone https://github.com/rust-lang/rust-mode.git
+git clone https://github.com/rust-lang/rfcs.git
+git clone https://github.com/rust-lang/rust-buildbot.git
+git clone https://github.com/rust-lang/rustup.git
+git clone https://github.com/rust-lang/rust-packaging.git
+git clone https://github.com/rust-lang/meeting-minutes.git
+git clone https://github.com/rust-lang/rust-installer.git
+git clone https://github.com/rust-lang/rust-wiki-backup.git
+git clone https://github.com/rust-lang/rust-playpen.git
+git clone https://github.com/rust-lang/hexfloat.git
+git clone https://github.com/rust-lang/gemalloc.git
+git clone https://github.com/rust-lang/rust-guidelines.git
+git clone https://github.com/rust-lang/fourcc.git
+git clone https://github.com/rust-lang/rust-www.git
+cd ../
+
