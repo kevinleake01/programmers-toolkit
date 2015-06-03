4bd0e51c6e40b89247d44d8898daa50413aa5262
kevinleake01 <kevinleake01@gmail.com>
2015-06-03 19:08:01 +0000

    Adding 12-bcpl

---
+++ 12-bcpl/setup-bcpl-linux.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+mkdir 01-bcpl
+cd 01-bcpl
+wget http://www.cl.cam.ac.uk/users/mr/BCPL/bcpl.tgz
+wget http://www.cl.cam.ac.uk/users/mr/BCPL/README
+wget http://www.cl.cam.ac.uk/users/mr/bcplman.pdf
+cd ../
+
+mkdir 02-bcpl64
+cd 02-bcpl64
+wget http://www.cl.cam.ac.uk/users/mr/BCPL64/bcpl64.tgz
+wget http://www.cl.cam.ac.uk/users/mr/BCPL64/README
+cd ../
+
+mkdir 03-mcpl
+cd 03-mcpl
+wget http://www.cl.cam.ac.uk/users/mr/MCPL/mcpl.tgz
+wget http://www.cl.cam.ac.uk/users/mr/mcplman.pdf
+cd ../
+
+mkdir 04-vspl
+cd 04-vspl
+wget http://www.cl.cam.ac.uk/users/mr/VSPL/vspl.tgz
+wget http://www.cl.cam.ac.uk/users/mr/VSPL/README
+cd ../
+
+mkdir 05-cintpos
+cd 05-cintpos
+wget http://www.cl.cam.ac.uk/users/mr/Cintpos/cintpos.tgz
+wget http://www.cl.cam.ac.uk/users/mr/Cintpos/README
+cd ../
+
+mkdir 06-bench
+cd 06-bench
+wget http://www.cl.cam.ac.uk/users/mr/Bench/bench.tgz
+wget http://www.cl.cam.ac.uk/users/mr/Bench/README
+cd ../
+
+mkdir 07-cobench
+cd 07-cobench
+wget http://www.cl.cam.ac.uk/users/mr/Cobench/cobench.tgz
+wget http://www.cl.cam.ac.uk/users/mr/Cobench/README
+cd ../
+
+mkdir 08-tcobench
+cd 08-tcobench
+wget http://www.cl.cam.ac.uk/users/mr/Tcobench/tcobench.tgz
+wget http://www.cl.cam.ac.uk/users/mr/Tcobench/README
+cd ../
+
+mkdir 09-nordier
+cd 09-nordier
+wget http://www.nordier.com/software/distfiles/obcpl-0.9.8.tar.xz
+wget http://www.nordier.com/software/distfiles/bcplkit-0.9.7.tar.xz
+wget http://www.nordier.com/software/distfiles/bcpl-0.8a.tar.bz2
+wget http://www.nordier.com/software/distfiles/bcpltape.tar.xz
+cd ../
+
