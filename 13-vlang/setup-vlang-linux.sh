a679d06b02df2bacdb20854a8c13dd128dee98f2
Kevin Leake <kevinleake01@gmail.com>
2020-07-09 11:47:49 +0000

    Update setup-vlang-linux.sh

--- 13-vlang/setup-vlang-linux.sh
+++ 13-vlang/setup-vlang-linux.sh
@@ -3,8 +3,8 @@
 # documentation and goodies to your computer.
 #
 # --- VLANG ---
-mkdir 01-v
-cd 01-v
+mkdir 01-vlang
+cd 01-vlang
 git clone git://github.com/vlang/v.git
 git clone git://github.com/vlang/ui.git
 git clone git://github.com/vlang/awesome-v.git
