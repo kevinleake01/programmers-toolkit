04286cfddbc0eedc34650fec372fb772dd33d785
Kevin Leake <kevinleake01@gmail.com>
2022-02-13 12:10:29 +0000

    Add files via upload

---
+++ 08-linux/batch-exec-t2sde.c
@@ -0,0 +1,26 @@
+/*
+####################################
+#
+# --- BATCH-EXEC-T2SDE.C ---
+#
+# This downloads T2 SDE Source Code and Handbook
+# using Subversion.
+#
+#
+# To run this program:
+#
+# gcc batch-exec-t2sde.c; ./a.out
+#
+####################################
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  system("svn co https://svn.exactcode.de/t2/tags/21.7 02-t2sde-21.7");
+  system("svn co https://svn.exactcode.de/t2/trunk 02-t2sde-trunk");
+  system("svn co https://svn.exactcode.de/t2-handbook/tags/0.1.7 02-t2sde-handbook-0.1.7");
+  system("svn co https://svn.exactcode.de/t2-handbook/trunk 02-t2sde-handbook-trunk");
+}
+
